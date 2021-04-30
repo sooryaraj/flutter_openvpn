5734c696e6d966e3c153942855d3fa4032489dd1
Ali Abdollahi <abdollahi890@gmail.com>
2021-04-30 21:54:17 +0430

    moved to null safety

--- lib/flutter_openvpn.dart
+++ lib/flutter_openvpn.dart
@@ -7,7 +7,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 
 typedef OnConnectionStatusChanged = Function(
-    String duration, String lastPacketRecieve, String byteIn, String byteOut);
+    String duration, String lastPacketReceive, String byteIn, String byteOut);
 
 /// Track vpn profile status.
 typedef OnProfileStatusChanged = Function(bool isProfileLoaded);
@@ -15,7 +15,7 @@ typedef OnProfileStatusChanged = Function(bool isProfileLoaded);
 /// Track Vpn status.
 ///
 /// Status strings are but not limited to:
-/// "CONNECTING", "CONNECTED", "DISCONNECTING", "DISCONNECTED", "TIMEOUT", "EXPIRED", "INVALID", "REASSERING", "AUTH", ...
+/// "CONNECTING", "CONNECTED", "DISCONNECTING", "DISCONNECTED", "TIMEOUT", "EXPIRED", "INVALID", "REASSERTING", "AUTH", ...
 /// print status to get full insight.
 /// status might change depending on platform.
 typedef OnVPNStatusChanged = Function(String status);
@@ -26,11 +26,24 @@ const String _vpnStatus = 'vpnStatus';
 const String _vpnStatusGroup = "vpnStatusGroup";
 const String _connectionId = "connectionId";
 
+class InitializationResult {
+  final String? vpnCurrentStatus;
+  final DateTime? latestExpireAt;
+  final String? latestConnectionName;
+  final String? latestConnectionId;
+  InitializationResult({
+    this.latestConnectionId,
+    this.latestConnectionName,
+    this.latestExpireAt,
+    this.vpnCurrentStatus,
+  });
+}
+
 class FlutterOpenvpn {
   static const MethodChannel _channel = const MethodChannel('flutter_openvpn');
-  static OnProfileStatusChanged _onProfileStatusChanged;
-  static OnVPNStatusChanged _onVPNStatusChanged;
-  static OnConnectionStatusChanged _onConnectionStatusChanged;
+  static OnProfileStatusChanged? _onProfileStatusChanged;
+  static OnVPNStatusChanged? _onVPNStatusChanged;
+  static OnConnectionStatusChanged? _onConnectionStatusChanged;
   static String _vpnState = "";
 
   /// Initialize plugin.
@@ -43,53 +56,36 @@ class FlutterOpenvpn {
   ///
   /// providerBundleIdentifier : Bundle id of your vpn extension.
   ///
-  /// returns {"currentStatus" : "VPN_CURRENT_STATUS",
-  ///
-  /// "expireAt" : "VPN_EXPIRE_DATE_STRING_IN_FORMAT(yyyy-MM-dd HH:mm:ss)",} if successful
-  ///
   ///  returns null if failed
-  static Future<dynamic> init(
-      {String providerBundleIdentifier,
-      String localizedDescription,
-      String groupIdentifier}) async {
-    if (Platform.isIOS) assert(groupIdentifier != null);
-    dynamic isInited = await _channel.invokeMethod("init", {
+  static Future<InitializationResult?> init(
+      {String? providerBundleIdentifier,
+      String? localizedDescription,
+      String? groupIdentifier}) async {
+    if (Platform.isIOS)
+      assert(
+        groupIdentifier != null &&
+            providerBundleIdentifier != null &&
+            localizedDescription != null,
+        "These values are required for ios.",
+      );
+    dynamic? initializeResult = await _channel.invokeMethod("init", {
       'localizedDescription': localizedDescription,
       'providerBundleIdentifier': providerBundleIdentifier,
     }).catchError((error) => error);
-    if (!(isInited is PlatformException) || isInited == null) {
-      /* _channel.setMethodCallHandler((call) {
-        switch (call.method) {
-          case _connectionUpdate:
-            _onConnectionStatusChanged?.call(
-                call.arguments['duration'],
-                call.arguments['lastPacketRecieve'],
-                call.arguments['byteIn'],
-                call.arguments['byteOut']);
-            break;
-          case _profileLoaded:
-            _onProfileStatusChanged?.call(true);
-            break;
-          case _profileLoadFailed:
-            _onProfileStatusChanged?.call(false);
-            break;
-          default:
-            _onVPNStatusChanged?.call(call.method);
-        }
-        return null;
-      }); */
-
+    if (initializeResult is! PlatformException || initializeResult == null) {
       StreamingSharedPreferences sp = StreamingSharedPreferences();
-      sp.setPrefsName("flutter_openvpn");
+      sp.setNativePreferencesName("flutter_openvpn");
       sp.addObserver(_connectionUpdate, (value) {
+        if (value == null) return;
         List<String> values = value.split('_');
         _onConnectionStatusChanged?.call(
-            values[0], values[1], values[2], value[3]);
+            values[0], values[1], values[2], values[3]);
       });
       sp.addObserver(_profile, (value) {
         _onProfileStatusChanged?.call(value == '0' ? false : true);
       });
       sp.addObserver(_vpnStatus, (value) {
+        if (value == null) return;
         _vpnState = value;
         _onVPNStatusChanged?.call(value);
       });
@@ -97,13 +93,15 @@ class FlutterOpenvpn {
 
       if (Platform.isIOS) {
         StreamingSharedPreferences spGroup = StreamingSharedPreferences();
-        spGroup.setPrefsName(groupIdentifier);
+        spGroup.setNativePreferencesName(groupIdentifier!);
         spGroup.addObserver(_connectionUpdate, (value) {
+          if (value == null) return;
           List<String> values = value.split('_');
           _onConnectionStatusChanged?.call(
-              values[0], values[1], values[2], value[3]);
+              values[0], values[1], values[2], values[3]);
         });
         spGroup.addObserver(_vpnStatusGroup, (value) {
+          if (value == null) return;
           _vpnState = value;
           _onVPNStatusChanged?.call(value);
         });
@@ -112,37 +110,48 @@ class FlutterOpenvpn {
 
       SharedPreferences spId = await SharedPreferences.getInstance();
       if (spId.containsKey(_connectionId)) {
-        List<String> splited = spId.getString(_connectionId).split('{||}');
-        isInited.putIfAbsent('connectionId', () => splited[1]);
-        isInited.putIfAbsent('connectionName', () => splited[0]);
+        List<String> spliced = spId.getString(_connectionId)!.split('{||}');
+        initializeResult?.putIfAbsent('connectionId', () => spliced[1]);
+        initializeResult?.putIfAbsent('connectionName', () => spliced[0]);
       }
-      return isInited;
+      return InitializationResult(
+        latestConnectionId:
+            initializeResult == null ? null : initializeResult['connectionId'],
+        latestConnectionName: initializeResult == null
+            ? null
+            : initializeResult['connectionName'],
+        latestExpireAt: DateTime.tryParse(initializeResult == null
+            ? ""
+            : (initializeResult['expireAt'] ?? "")),
+        vpnCurrentStatus:
+            initializeResult == null ? null : initializeResult['currentStatus'],
+      );
     } else {
-      print('OpenVPN Initilization failed');
-      print((isInited as PlatformException).message);
-      print((isInited as PlatformException).details);
+      print('OpenVPN Initialization failed');
+      print(initializeResult.message);
+      print(initializeResult.details);
       return null;
     }
   }
 
-  static Future<String> _currentCon() async =>
+  static Future<String?> _currentCon() async =>
       (await SharedPreferences.getInstance()).getString(_connectionId);
-  static Future<String> getVpnStatus() async {
+  static Future<String?> getVpnStatus() async {
     try {
       return await _channel.invokeMethod<String>("getStatus");
     } catch (err) {
-      return "";
+      return null;
     }
   }
 
-  static Future<String> get currentProfileId async {
-    List<String> vars = (await _currentCon())?.split('{||}');
+  static Future<String?> get currentProfileId async {
+    List<String>? vars = (await _currentCon())?.split('{||}');
     if (vars == null) return null;
     return vars.last;
   }
 
-  static Future<String> get currentProfileName async {
-    List<String> vars = (await _currentCon())?.split('{||}');
+  static Future<String?> get currentProfileName async {
+    List<String>? vars = (await _currentCon())?.split('{||}');
     if (vars == null || vars.length != 2) return null;
     return vars.first;
   }
@@ -151,16 +160,16 @@ class FlutterOpenvpn {
   ///
   /// if expireAt is provided
   /// Vpn session stops itself at given date.
-  static Future<int> lunchVpn(
-    String ovpnFileContents,
-    OnProfileStatusChanged onProfileStatusChanged,
-    OnVPNStatusChanged onVPNStatusChanged, {
-    DateTime expireAt,
+  static Future<int> lunchVpn({
+    required String ovpnFileContents,
+    OnProfileStatusChanged? onProfileStatusChanged,
+    OnVPNStatusChanged? onVPNStatusChanged,
+    DateTime? expireAt,
     String user = '',
     String pass = '',
-    OnConnectionStatusChanged onConnectionStatusChanged,
-    String connectionName,
-    String connectionId,
+    OnConnectionStatusChanged? onConnectionStatusChanged,
+    String connectionName = '',
+    String connectionId = '',
     Duration timeOut = const Duration(seconds: 60),
   }) async {
     _onProfileStatusChanged = onProfileStatusChanged;
@@ -173,13 +182,12 @@ class FlutterOpenvpn {
       "lunch",
       {
         'ovpnFileContent': ovpnFileContents,
-        'user': user ?? "",
-        'pass': pass ?? "",
-        'conName': connectionName ?? "",
-        'conId': connectionId ?? "",
-        'timeOut': Platform.isIOS
-            ? timeOut?.inSeconds?.toString()
-            : timeOut?.inSeconds,
+        'user': user,
+        'pass': pass,
+        'conName': connectionName,
+        'conId': connectionId,
+        'timeOut':
+            Platform.isIOS ? timeOut.inSeconds.toString() : timeOut.inSeconds,
         'expireAt': expireAt == null
             ? null
             : DateFormat("yyyy-MM-dd HH:mm:ss").format(expireAt),
@@ -187,7 +195,7 @@ class FlutterOpenvpn {
     ).catchError((error) => error);
     if (isLunched == null) return 0;
     print((isLunched as PlatformException).message);
-    return int.tryParse((isLunched as PlatformException).code);
+    return int.tryParse(isLunched.code) ?? -1;
   }
 
   /// stops any connected session.
